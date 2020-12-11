8dee326887328ddc66d529a654ed8021b9513e85
DeltaMod <33879770+DeltaMod@users.noreply.github.com>
2020-12-11 02:59:58 +0100

    Update Laser_Calc_Funcs.py

--- FAFN14_YAG/Laser_Calc_Funcs.py
+++ FAFN14_YAG/Laser_Calc_Funcs.py
@@ -16,42 +16,63 @@ def Func_E_Cap(C,V):
 
 E_Cap = Func_E_Cap(C,V_pump)
 
-dur = {'IR_nQ':np.array([1]),
-       'IR_Q':np.array([1]),
-       '532_Q':np.array([1])} 
+data = {'IR_NQ':{},'IR_Q':{},'532_Q':{}}
 
-pow_av = {'IR_nQ':np.array([1]),
-          'IR_Q':np.array([1]),
-          '532_Q':np.array([1])} 
+for key in data.keys():
+    L = 20
+    data[key] = {'V_pump' : np.array([]),
+                 'E_cap'  : np.array([]),
+                 'pow_avg': np.array([]),
+                 'E_Pulse': np.array([]),
+                 'eff'    : np.array([]),
+                 'P_peak' : np.array([]),
+                 
+                 't_FWHM' : np.array([]),
+                 'C':30*10**-6,
+                 'rate':10,
+                 'duration': None,
+                 'LT':None,
+                 'f_prof':0.94}
 
-E_pulse  = {'IR_nQ':np.array([1]),
-            'IR_Q':np.array([1]),
-            '532_Q':np.array([1])} 
+M = {'Cu':   {'EV':4.73 * 10**6,'MD':8960}, #J/kg, and kg/m^3
+     'Al':   {'EV':10.53* 10**6,'MD':2700},
+     'Steel':{'EV':6.80 * 10**6,'MD':8050},
+     'Fe':   {'EV':6.09 * 10**6,'MD':7874},
+     'Brass':{'EV':4.73 * 10**6,'MD':8730}}
 
-eff = {'IR_nQ':np.array([1]),
-       'IR_Q':np.array([1]),
-       '532_Q':np.array([1])} 
 
-pow_peak = {'IR_nQ':np.array([1]),
-            'IR_Q':np.array([1]),
-            '532_Q':np.array([1])} 
 
-t_FWHM = {'IR_nQ':np.array([1]),
-            'IR_Q':np.array([1]),
-            '532_Q':np.array([1])} 
+def Func_PulseEnergy(Pow,Rep):
+    E_pulse = Pow/Rep
+    return(E_pulse)
 
-LT = ['IR_nQ','IR_Q','532_Q'] 
-
-f_prof = 0.94
-
-def Func_PeakPow(t_FWHM, f_prof, E_p):
-    
-    P_p = f_prof*E_p/t_FWHM
+def Func_PeakPow(t_FWHM, f_prof, E_pul):
+    P_p = f_prof*E_pul/t_FWHM
     
     return(P_p)
 
+def Func_CapEnergy(Cap,Volt):
+    E_cap = Cap*np.power(Volt,2)/2
+    return(E_cap)
+    
+def Func_Efficiency(P_in,P_out):
+    Eff = P_out/P_in
+    return(Eff)
 
-for cond in LT:
-    pow_peak[cond] = Func_PeakPow(t_FWHM[cond], f_prof, [0])
+for key in data.keys():
+    data[key]['E_Pulse'] = Func_PulseEnergy(data[key]['pow_avg'],data[key]['rate'])
+    data[key]['P_peak']  = Func_PeakPow(data[key]['t_FWHM'], data[key]['f_prof'], data[key]['E_Pulse'])
+    data[key]['E_cap']   = Func_CapEnergy(data[key]['C'],data[key]['V_pump'])
+    data[key]['eff']     = Func_Efficiency(data[key]['P_peak'],data[key]['pow_avg']) #Check later
+    
+
+
+for key in data.keys():
+    print(key+'\n==============')
+    STRL = len(max(data['IR_NQ'].keys(),key=len))
+    for subkey in data[key].keys():
+        pad = [' ' for n in range(STRL-len(subkey))]
+        
+        print(subkey+"".join(pad)+str(data[key][subkey]))
 
     
